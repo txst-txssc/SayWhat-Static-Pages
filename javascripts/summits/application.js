7460901ecee20b6a703cfc1a92e4a8654485b35c
Cody Stoltman <particlebanana@gmail.com>
2012-10-01 16:09:11 -0500

    add summit page

---
+++ javascripts/summits/application.js
@@ -0,0 +1,12 @@
+$('document').ready(function() {
+
+  $('#featured').orbit({
+    animation: 'horizontal-slide',   // fade, horizontal-slide, vertical-slide, horizontal-push
+    animationSpeed: 800,             // how fast animtions are
+    timer: true,                    // true or false to have the timer
+    advanceSpeed: 4000               // if timer is enabled, time between transitions 
+  });
+
+  $('.small').fadeIn('fast');
+
+});
